fb35f3293b59cc5b36210b34543e291b8391831b
alu0101105741 <alu0101105741@ull.edu.es>
2021-04-08 11:30:05 +0100

    Ejercicio implementado con map (Fallo en el print)

--- src/ejercicio.ts
+++ src/ejercicio.ts
@@ -6,56 +6,71 @@ interface LIFOInterface<T> {
 }
 
 /**
- * asd
+ * Class that implement a LIFO with a generic interface
  */
-export class LIFO<T> implements LIFOInterface<T> {
-  private values: T[];
+export class LIFO<T> implements LIFOInterface<T> implements Iterable<T> {
+  private values: Map<number, T>;
+  private index: number = 1;
 
   /**
-   * asd
-   * @param {T} values asd
+   * Constructor function of the LIFO class
+   * @param {T} values Values given to the constructor to initialice the class
    */
   constructor(values: T[]) {
-    this.values = values;
+    this.values = new Map<number, T>();
+    values.forEach((value) => this.push(value));
   }
 
   /**
-   * asd
-   * @param {T} newElement asd
+   * Push function of the LIFO class
+   * @param {T} newElement Element given to add into the LIFO class
    */
   push(newElement: T) {
-    this.values.push(newElement);
+    this.values.set(this.index, newElement);
+    this.index = this.index + 1;
   }
 
   /**
-   * asd
-   * @return {T} asd
+   * Pop function of the LIFO class
+   * @return {T} Value of the last element,
+   * the one that is remove from the class
    */
   pop(): T {
-    return this.values.pop();
+    const dummy: T = this.values.get(this.index - 1);
+    this.values.delete(this.index - 1);
+    this.index = this.index - 1;
+    return dummy;
   }
 
   /**
-   * asd
-   * @return {T} asd
+   * Pseek function of the LIFO class
+   * @return {T} Value of the last element,
+   * in this case this value is not remove from the class
    */
   pseek(): T {
-    return this.values[this.values.length - 1];
+    return this.values.get(this.index - 1);
   }
 
   /**
-   * asd
-   * @return {number} asd
+   * Size function of the LIFO class
+   * @return {number} Number of elements inside the class
    */
   size(): number {
-    return this.values.length;
+    return this.values.size;
   }
 
   /**
-   * asd
-   * @return {string} asd
+   * Making our function iterable
+   * @return {Iterator<T>} asd
+   */
+  [Symbol.iterator](): Iterator<T> {
+    return this.values.values();
+  }
+
+  /**
+   * Print function of the LIFO class
    */
   print(): string {
-    return this.values.toString();
+    this.values.forEach((element) => element.print());
   }
 }
